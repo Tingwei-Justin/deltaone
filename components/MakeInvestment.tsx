287bf041b746729d31d1d5cea1abe9f5f43fb63e
Home <Home@MacBook-Pro.local>
2021-10-15 10:20:14 -0700

    invest is only for beta users.

--- components/MakeInvestment.tsx
+++ components/MakeInvestment.tsx
@@ -111,6 +111,7 @@ const MakeInvestment = ({
               <button
                 type="submit"
                 className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
+                onClick={()=>alert("Only for beta users. Join Discord and ask to join.")}
               >
                 Invest
               </button>
