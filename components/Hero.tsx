7ba936ab42a9e4bb00e072a0117c948324c47964
Home <Home@MacBook-Pro.local>
2021-10-15 00:15:11 -0700

    highlight 1%

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,6 +38,42 @@ const Hero = () => {
         </div>
       </main>
       <div className="py-12 ">
+  <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
+    <div className="max-w-4xl mx-auto text-center">
+      <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+        Trusted by users across the world.
+      </h2>
+      <p className="mt-3 text-xl text-indigo-400 sm:mt-4">
+        Our closed beta users are reaping the benefits.
+      </p>
+    </div>
+    <dl className="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
+      <div className="flex flex-col">
+        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-indigo-400">
+          TVL
+        </dt>
+        <dd className="order-1 text-5xl font-extrabold text-white">
+          $150K+
+        </dd>
+      </div>
+      <div className="flex flex-col mt-10 sm:mt-0">
+        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-indigo-400">
+          Yield per day
+        </dt>
+        <dd className="order-1 text-5xl font-extrabold text-yellow-400">
+          1%
+        </dd>
+      </div>
+      <div className="flex flex-col mt-10 sm:mt-0">
+        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-indigo-400">
+          Wallets
+        </dt>
+        <dd className="order-1 text-5xl font-extrabold text-white">
+          292
+        </dd>
+      </div>
+    </dl>
+</div>
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-24">
     <div className="lg:text-center">
       <h2 className="text-base text-indigo-400 font-semibold tracking-wide uppercase">DELTA ONE PROTOCOL</h2>
@@ -110,42 +146,6 @@ const Hero = () => {
       </dl>
     </div>
   </div>
-</div>
-  <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
-    <div className="max-w-4xl mx-auto text-center">
-      <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-        Trusted by users across the world.
-      </h2>
-      <p className="mt-3 text-xl text-indigo-400 sm:mt-4">
-        Our closed beta users are reaping the benefits.
-      </p>
-    </div>
-    <dl className="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
-      <div className="flex flex-col">
-        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-indigo-400">
-          TVL
-        </dt>
-        <dd className="order-1 text-5xl font-extrabold text-white">
-          $150K+
-        </dd>
-      </div>
-      <div className="flex flex-col mt-10 sm:mt-0">
-        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-indigo-400">
-          Yield per day
-        </dt>
-        <dd className="order-1 text-5xl font-extrabold text-white">
-          1%
-        </dd>
-      </div>
-      <div className="flex flex-col mt-10 sm:mt-0">
-        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-indigo-400">
-          Wallets
-        </dt>
-        <dd className="order-1 text-5xl font-extrabold text-white">
-          292
-        </dd>
-      </div>
-    </dl>
 </div>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
     <div className="max-w-4xl mx-auto text-center">
