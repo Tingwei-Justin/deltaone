e9b7b46df2ed569721269e47299cf9b23c0fc997
Home <Home@MacBook-Pro.local>
2021-10-14 23:58:44 -0700

    stablecoin

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,18 +31,86 @@ const Hero = () => {
                 <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
                   Delta one protocol is a stablecoin which earns yield. It gives access to high DeFi yields through a continuously optimised portfolio of advanced hedged yield farming strategies.
                 </p>
-                <p className="mt-8 text-sm text-white tracking-wide font-semibold sm:mt-10">
-                  Delta One Protocol returns are higher because they are leveraged using assets. 
-                  The protocol uses delta hedging so losses are covered. 
-                  Delta One is a stablecoin protocol and the risks are primarily smart contract and stablecoin risks, rather than price volatility.
-
-                </p>
 
               </div>
             </div>
           </div>
         </div>
       </main>
+      <div className="py-12 ">
+  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="lg:text-center">
+      <h2 className="text-base text-indigo-400 font-semibold tracking-wide uppercase">DELTA ONE PROTOCOL</h2>
+      <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+        A better way to hold your money.
+      </p>
+      <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+      Delta One Protocol returns are higher because they are leveraged using assets. 
+      </p>
+    </div>
+
+    <div className="mt-10">
+      <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+        <div className="relative">
+          <dt>
+            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+              </svg>
+            </div>
+            <p className="ml-16 text-lg leading-6 font-medium text-white">Stablecoin</p>
+          </dt>
+          <dd className="mt-2 ml-16 text-base text-gray-500">
+
+          Because Delta One preserves its value and pegs itself to the USDC, you can feel safe using it.
+          </dd>
+        </div>
+
+        <div className="relative">
+          <dt>
+            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+              </svg>
+            </div>
+            <p className="ml-16 text-lg leading-6 font-medium text-white">Decenteralized</p>
+          </dt>
+          <dd className="mt-2 ml-16 text-base text-gray-500">
+            Delta One Protocol will be decenteralized and goverened using Delta One DAO. 
+          </dd>
+        </div>
+
+        <div className="relative">
+          <dt>
+            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+              </svg>
+            </div>
+            <p className="ml-16 text-lg leading-6 font-medium text-white">Hedged</p>
+          </dt>
+          <dd className="mt-2 ml-16 text-base text-gray-500">
+          The protocol uses delta hedging so losses are covered. 
+          </dd>
+        </div>
+
+        <div className="relative">
+          <dt>
+            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+              </svg>
+            </div>
+            <p className="ml-16 text-lg leading-6 font-medium text-white">Safe and secure</p>
+          </dt>
+          <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Delta One is a stablecoin protocol and the risks are primarily smart contract and stablecoin risks, rather than price volatility.
+          </dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</div>
   <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
     <div className="max-w-4xl mx-auto text-center">
       <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
